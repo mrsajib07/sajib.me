c852b351cda6156478201bd9bb6f7c094e9f3784
mrsajib07 <bsajib116@gmail.com>
2025-12-03 00:37:34 +0530

    up again

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# sajib.me
+# PortfolioTemplate-11
 
+A responsive personal portfolio template using only HTML. Perfect for showcasing skills and projects.
+
+🔗 **Live Demo**: [Click Here](https://engrshuvodas.github.io/PortfolioTemplate-11/)
---
+++ js/main.js
@@ -0,0 +1,222 @@
+/* Author : Pranto Kumar */
+
+
+// Navigation bar effects on scroll
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+
+// services section - Modal
+const serviceModals = document.querySelectorAll(".service-modal");
+const learnmoreBtns = document.querySelectorAll(".learn-more-btn");
+const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
+
+var modal = function (modalClick) {
+  serviceModals[modalClick].classList.add("active");
+}
+
+learnmoreBtns.forEach((learnmoreBtns, i) => {
+  learnmoreBtns.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalCloseBtns.forEach((modalCloseBtns) => {
+  modalCloseBtns.addEventListener("click", () => {
+    serviceModals.forEach((modalview) => {
+      modalview.classList.remove("active");
+    });
+  });
+});
+
+
+
+
+
+// Portfolio section - Modal
+const portfolioModals = document.querySelectorAll(".portfolio-model");
+const imgCards = document.querySelectorAll(".img-card");
+const portfolioCloseBtns = document.querySelectorAll(".portfolio-close-btn");
+
+var portfolioModal = function (modalClick) {
+  portfolioModals[modalClick].classList.add("active");
+}
+
+imgCards.forEach((imgCard, i) => {
+  imgCard.addEventListener("click", () => {
+    portfolioModal(i);
+  });
+});
+
+portfolioCloseBtns.forEach((portfolioCloseBtn) => {
+  portfolioCloseBtn.addEventListener("click", () => {
+    portfolioModals.forEach((portfolioModalview) => {
+      portfolioModalview.classList.remove("active");
+    });
+  });
+});
+
+
+
+
+
+
+// Our clients - Swiper
+var swiper = new Swiper(".client-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+
+/*=============== Preloader ===============*/
+
+var loader = document.getElementById("preloader");
+
+window.addEventListener("load", function () {
+    loader.style.display = "none";
+})
+
+
+
+
+
+
+// Website dark/light theme
+const themeBtn = document.querySelector(".theme-btn");
+
+themeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+  themeBtn.classList.toggle("sun");
+
+  localStorage.setItem("saved-theme", getCurrentTheme());
+  localStorage.setItem("saved-icon", getCurrenticon());
+});
+
+
+const getCurrentTheme = () => document.body.classList.contains("dark-them") ? "dark" : "light";
+const getCurrenticon = () => themeBtn.classList.contains("sun") ? "sun" : "moon";
+
+const saveTheme = localStorage.getItem("save-theme");
+const saveIcon = localStorage.getItem("saved-icon");
+
+if (saveTheme) {
+  document.body.classList[saveTheme === "dark" ? "add" : "remove"]("dark-theme");
+  themeBtn.classList[saveIcon === "sun" ? "add" : "remove"]("sun");
+}
+
+
+
+
+
+
+
+// Scroll to top button
+const scrollToBtn = document.querySelector(".scrollToTop-btn");
+window.addEventListener("scroll", function () {
+  scrollToBtn.classList.toggle("active", window.scrollY > 500);
+});
+
+scrollToBtn.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+
+
+
+
+
+
+// Navigation menu items active on page scroll
+window.addEventListener("scroll", () => {
+  const sections = document.querySelectorAll("section");
+  let scrollY = window.pageYOffset;
+
+  sections.forEach(current => {
+    let sectionHeight = current.offsetHeight;
+    let sectionTop = current.offsetTop - 50;
+    let Id = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector(".nav-items a[href*=" + Id + "]").classList.add("active");
+    }
+    else {
+      document.querySelector(".nav-items a[href*=" + Id + "]").classList.remove("active");
+    }
+
+  });
+
+});
+
+
+
+
+
+// Responsive navigation menu toggle
+const menuBtn = document.querySelector(".nav-menu-btn");
+const closeBtn = document.querySelector(".nav-close-btn");
+const navigation = document.querySelector(".navigation");
+const navItems = document.querySelectorAll(".nav-items a");
+
+menuBtn.addEventListener("click", () => {
+  navigation.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  navigation.classList.remove("active");
+});
+
+navItems.forEach((navItem) => {
+  navItem.addEventListener("click", () => {
+    navigation.classList.remove("active");
+  });
+});
+
+
+
+
+
+
+
+
+// Scroll reveal animations
+// Common reveal options to create reveal animations
+ScrollReveal({
+  reset: true,
+  distance: '60px',
+  duration: 2500,
+  delay: 100
+});
+
+
+// Target elements, and specify options to create reveal animations
+ScrollReveal().reveal('.home .info h2,.section-title-01, .section-title-02', { delay: 500, origin: 'left' });
+
+ScrollReveal().reveal('.home .info h3, .home .info p, .about-info .btn', { delay: 600, origin: 'right' });
+
+ScrollReveal().reveal('.home .info .btn', { delay: 700, origin: 'bottom' });
+
+ScrollReveal().reveal('.media-icons i, .contact-left li', { delay: 500, origin: 'left', interval: 200 });
+
+ScrollReveal().reveal('.home-img, .about-img', { delay: 500, origin: 'bottom' });
+
+ScrollReveal().reveal('.about, .description, .contact-right', { delay: 600, origin: 'right' });
+
+ScrollReveal().reveal('.about, .professional-list li', { delay: 500, origin: 'right', interval: 200 });
+
+ScrollReveal().reveal('.skills-description, .services-description, .contact-card, .client-swiper, .contact-left h2', { delay: 700, origin: 'left' });
+
+ScrollReveal().reveal('.experience-card, .service-card, .education, .portfolio .img-card', { delay: 800, origin: 'bottom', interval: 200 });
+
+ScrollReveal().reveal('footer .group', { delay: 500, origin: 'top', interval: 200 });
